cdd00b28359555d2ef77dce3ae0efbfc0f282013
Arka Das <arkadas1411@gmail.com>
2026-04-07 02:08:13 +0530

    fixed model file paths

--- ml_service.py
+++ ml_service.py
@@ -1,8 +1,32 @@
-import joblib
+# import joblib
 
-priority_model = joblib.load("priority_model.pkl")
-department_model = joblib.load("department_model.pkl")
-category_model = joblib.load("category_model.pkl")
+# priority_model = joblib.load("priority_model.pkl")
+# department_model = joblib.load("department_model.pkl")
+# category_model = joblib.load("category_model.pkl")
+
+# def classify_complaint(text: str):
+#     priority = priority_model.predict([text])[0]
+#     department = department_model.predict([text])[0]
+#     category = category_model.predict([text])[0]
+
+#     return department, category, priority
+
+import joblib
+import os
+
+# Get correct base directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Build full paths
+priority_path = os.path.join(BASE_DIR, "priority_model.pkl")
+department_path = os.path.join(BASE_DIR, "department_model.pkl")
+category_path = os.path.join(BASE_DIR, "category_model.pkl")
+
+# Load models
+priority_model = joblib.load(priority_path)
+department_model = joblib.load(department_path)
+category_model = joblib.load(category_path)
+
 
 def classify_complaint(text: str):
     priority = priority_model.predict([text])[0]
